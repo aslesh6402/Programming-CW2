450dd949aec64654b8e4972b7d2bac8d32f0dcfe
aslesh6402 <adhikariaslesh@gmail.com>
2024-02-13 14:25:20 +0545

    start

--- main.py
+++ main.py
@@ -1,1 +1,7 @@
-import os 
+import tkinter as tk
+from tkinter import scrolledtext, messagebox, Checkbutton, IntVar, Entry, Label, Frame, Button, filedialog, simpledialog
+from bs4 import BeautifulSoup
+import requests
+import csv
+import json
+import re
