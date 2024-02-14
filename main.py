e1f5ab2036d1b34e00262d4db7429d56000111be
aslesh6402 <adhikariaslesh@gmail.com>
2024-02-14 13:50:02 +0545

    scrape

--- main.py
+++ main.py
@@ -5,3 +5,20 @@ import requests
 import csv
 import json
 import re
+
+def scrape():
+    url = url_entry.get()
+    content = ""
+    try:
+        response = requests.get(url)
+        soup = BeautifulSoup(response.text, 'html.parser')
+
+        # Scrape according to selected options
+        if headline_var.get():
+            content += "Headlines:\n" + '\n'.join([headline.text for headline in soup.find_all(['h1', 'h2', 'h3'])]) + "\n\n"
+        
+        if paragraph_var.get():
+            content += "Paragraphs:\n" + '\n\n'.join([p.text for p in soup.find_all('p')]) + "\n\n"
+        
+        if link_var.get():
+            content += "Links
